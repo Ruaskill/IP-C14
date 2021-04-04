d65314c7f0a5295190c6c065188071df6bc7785a
Ruaskill <69593117+Ruaskill@users.noreply.github.com>
2021-04-04 21:36:45 +0100

    Initial commit

---
+++ src/connectivity/ConnectionClass.java
@@ -0,0 +1,29 @@
+package connectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionClass {
+    public Connection connection;
+    public  Connection getConnection() {
+
+
+        String dbName="ipc14";
+        String userName="root";
+        String password="root";
+        String url = "jdbc:mysql://127.0.0.1:3306/ipc14";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+            connection = DriverManager.getConnection(url ,userName,password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return connection;
+    }
+
+}
